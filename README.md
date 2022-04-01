6bd509dccea9d687cff7a19ccb6c78a4c9ff3574
molly-muffin <laureendademeule@gmail.com>
2022-04-01 02:10:08 +0200

    Last commit

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 # Page de génération de playlists de films
 
 ## Contexte du projet : 
-Une association m'a contactée afin de créer une page HTML qui génererait des playlists de films en fonction de catégories.
+Une association m'a contactée afin de créer une page HTML qui génererait des playlists de films.
 
 ## But du projet : 
-Répertorier et classer des films suivant différentes informations et catégories. Afficher les catégories de films dans des caroussels
+Répertorier et classer des films en fonction de leur catégorie et de leur score Imdb. Afficher les catégories de films dans des caroussels.
 
 ### Statut du projet : 
 | Etapes | Avancement |
 | ------ | ------ |
-| index.html | **En cours** |
-| style.css | **En cours** |
-| recherche dans l'API | **En cours** |
-| Documenter la page | **En cours** |
+| index.html | **Terminé** |
+| style.css | **Terminé** |
+| script.js | **Terminé** |
+| README.md | **Terminé** |
+| Documenter le code | **Terminé** |
 
 ### La page permet :
-*	D'afficher le caroussel des "Meilleurs films", avec la jaquette du film, le résumé, ainsi qu'un bouton permettant d'afficher les différentes informations.
-*	D'afficher le caroussel des "Films les mieux notés", avec les jaquettes des 7 meilleurs films toutes catégories confondues. 
+*	D'afficher le "Meilleur film", avec la jaquette du film, le résumé, ainsi qu'un bouton permettant d'afficher les différentes informations.
+*	D'afficher le caroussel des "Films les mieux notés", avec les jaquettes des 4 meilleurs films toutes catégories confondues. 
 *	D'afficher les caroussels de 3 autres catégories au choix du développeur.
 ==>	Des boutons doivent permettre de faire défiler les films de gauche à droite, et chaque clic sur une jaquette doit afficher les informations du film dans une fenêtre.
 
@@ -29,14 +30,16 @@ Le site fonctionne sur les 3 navigateurs les plus utilisés actuellement (Chrome
 ### Instruction d’installation et d’utilisation :
 *	Prérequis
 	1. Copier les différents dossier et fichier contenu dans le repository dans un nouveau dossier.
-	2. Afin de pouvoir afficher les différentes informations de chaque film, vous devez installer l'API locale : https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR/blob/master/README.md
-	3. Démarrer l'API
+	==> `git clone https://github.com/molly-muffin/P6_OC_JustStreamIt.git` 
+	2. Afin de pouvoir afficher les différentes informations de chaque film, vous devez installer puis démarrer l'API enlocale
+	==> https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR/blob/master/README.md
 
 *	Lancement
 	Double-clic sur le fichier index.html
 
 ## Ce que j'ai appris :
+J'ai appris à construire l'infrastructure de base d'une page web grâce au `HTML` et à appliquer des styles afin de mettre en forme la page grâce au `CSS`.
+J'ai aussi appris à requêter une API grâce à fetch et à dinamiser mon site grâce à `Javascript` et les fonctions asynchrone.
 
-
-> Laureenda Demeul
+> Laureenda Demeule
 > OpenClassroom
--- scripts.js
+++ scripts.js
@@ -1,257 +1,260 @@
+/////////////////
+/// VARIABLES ///
+/////////////////
+
 const mainEntryUrl = "http://localhost:8000/api/v1/titles/";
-let movie_image_url = "";
-let movie_datas = [];
-let movie_datas_html = [];
-let movies_instances = [];
 
 
-function createFilm(filmId) {
-    let film = document.getElementById("myFilm");
-    let span = document.getElementsByClassName("close")[0];
-    film.style.display = "block";
+/////////////////
+/// FONCTIONS ///
+/////////////////
 
-    fetch(mainEntryUrl + filmId)
-      .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-      .then(function(data) {
-
-        //Ouvre la fenêtre filme et copie les informations du film à l'intérieur.
-    let film_img_el = document.getElementsByClassName("film_img")[0];
-    film_img_el.innerHTML = "<p><img src='" +  data.image_url + "'></p>";
-    let film_info_el = document.getElementsByClassName("film_infos");
-
-    let title_li = document.createElement("li");
-    title_li.innerHTML = "<em>Titre : </em>" + data.title;
-    film_info_el[0].appendChild(title_li);
-
-    let genres_li = document.createElement("li");
-    genres_li.innerHTML = "<em>Genre(s) : </em>" + data.genres;
-    film_info_el[0].appendChild(genres_li);
-
-    let year_li = document.createElement("li");
-    year_li.innerHTML = "<em>Date de sortie : </em>" + data.published;
-    film_info_el[0].appendChild(year_li);
-
-    let imdb_score_li = document.createElement("li");
-    imdb_score_li.innerHTML = "<em>Score IMBD : </em>" + data.imdb_score;
-    film_info_el[0].appendChild(imdb_score_li);
-
-    let directors_li = document.createElement("li");
-    directors_li.innerHTML = "<em>Réalisateur(s) : </em>" + data.directors;
-    film_info_el[0].appendChild(directors_li);
-
-    let actors_li = document.createElement("li");
-    actors_li.innerHTML = "<em>Acteurs : </em>" + data.actors;
-    film_info_el[0].appendChild(actors_li);
-
-    let duration_li = document.createElement("li");
-    duration_li.innerHTML = "<em>Durée : </em>" + data.duration + " minutes.";
-    film_info_el[0].appendChild(duration_li);
-
-    let country_li = document.createElement("li");
-    country_li.innerHTML = "<em>Pays d'origine : </em>" + data.country_li;
-    film_info_el[0].appendChild(country_li);
-
-    let box_office_results = document.createElement("li");
-    box_office_results.innerHTML = "<em>Box-office : </em>" + data.worldwide_gross_income;
-    film_info_el[0].appendChild(box_office_results);
-
-    let description_li = document.createElement("li");
-    description_li.innerHTML = "<em>Résumé : </em>" + data.description;
-    film_info_el[0].appendChild(description_li);
-
-    // Ferme la fenêtre quand l'utilisateur clique sur "X", et efface les données.
-    span.onclick = function() {
-    film.style.display = "none";
-    let film_img = document.getElementsByClassName("film_img")[0];
-    film_img.innerHTML = "";
-    let film_data = document.getElementsByClassName("film_infos")[0];
-    film_data.innerHTML = "";
-    }
-
-      // Ferme la fenêtre quand l'utilisateur clique en dehors de la fenêtre, et efface les données.
-    window.onclick = function(event) {
-    if (event.target == film) {
-      film.style.display = "none";
-      let film_img = document.getElementsByClassName("film_img")[0];
-      film_img.innerHTML = "";
-      let film_data = document.getElementsByClassName("film_infos")[0];
-      film_data.innerHTML = "";  
-        }
+// Création de la fenêtre pour les infos d'un film
+function createMovie(MovieId) {
+  let movie = document.getElementById("myMovie");
+  let span = document.getElementsByClassName("close")[0];
+  movie.style.display = "inline-table";
+  fetch(mainEntryUrl + MovieId)
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
       }
   })
-    .catch(function(error) {
-      console.error('Error:', error);
-  });
-};
+    // Ouvre la fenêtre d'un film et affiche les informations du film à l'intérieur
+    .then(function(data) {
+  // Image du film
+  let movie_img = document.getElementsByClassName("movie_img")[0];
+  movie_img.innerHTML = "<p><img src='" +  data.image_url + "'></p>";
+  let movie_info_el = document.getElementsByClassName("movie_infos");
+  // Titre du film
+  let title_li = document.createElement("li");
+  title_li.innerHTML = "<em>Titre : </em>" + data.title;
+  movie_info_el[0].appendChild(title_li);
+  // Genre(s) du film
+  let genres_li = document.createElement("li");
+  genres_li.innerHTML = "<em>Genre(s) : </em>" + data.genres;
+  movie_info_el[0].appendChild(genres_li);
+  // Année de sortie du film
+  let year_li = document.createElement("li");
+  year_li.innerHTML = "<em>Année de sortie : </em>" + data.year;
+  movie_info_el[0].appendChild(year_li);
+  // Score IMBD du film
+  let imdb_score_li = document.createElement("li");
+  imdb_score_li.innerHTML = "<em>Score IMBD : </em>" + data.imdb_score + " étoiles";
+  movie_info_el[0].appendChild(imdb_score_li);
+  // Réalisateur(s) du film
+  let directors_li = document.createElement("li");
+  directors_li.innerHTML = "<em>Réalisateur(s) : </em>" + data.directors;
+  movie_info_el[0].appendChild(directors_li);
+  // Acteurs du film
+  let actors_li = document.createElement("li");
+  actors_li.innerHTML = "<em>Acteurs : </em>" + data.actors;
+  movie_info_el[0].appendChild(actors_li);
+  // Durée du film
+  let duration_li = document.createElement("li");
+  duration_li.innerHTML = "<em>Durée : </em>" + data.duration + " minutes";
+  movie_info_el[0].appendChild(duration_li);
+  // Résumé du film
+  let description_li = document.createElement("li");
+  description_li.innerHTML = "<em>Résumé : </em>" + data.description;
+  movie_info_el[0].appendChild(description_li);
+  
+  // Ferme la fenêtre quand l'utilisateur clique sur "X", et efface les données.
+  span.onclick = function() {
+  movie.style.display = "none";
+  let movie_img = document.getElementsByClassName("movie_img")[0];
+  movie_img.innerHTML = "";
+  let movie_data = document.getElementsByClassName("movie_infos")[0];
+  movie_data.innerHTML = "";
+  }
+
+  // Ferme la fenêtre quand l'utilisateur clique en dehors de la fenêtre, et efface les données.
+  window.onclick = function(event) {
+  if (event.target == movie) {
+    movie.style.display = "none";
+    let movie_img = document.getElementsByClassName("movie_img")[0];
+    movie_img.innerHTML = "";
+    let movie_data = document.getElementsByClassName("movie_infos")[0];
+    movie_data.innerHTML = "";  
+      }
+    }
+  })
+
+  // Afficher l'erreur
+  .catch(function(error) {
+    console.error('Error:', error);
+  })
+}
 
 
 // Création de la fenêtre "meilleur film"
 function showPreviewBestMovie(url) {
   fetch(url)
-      .then(function(res) {
-          if (res.ok) {
-              return res.json();
-          }
-      })
-      .then(function(data) {
-      let best_movie_img = document.getElementById("best_movie_image");
-
-      best_movie_img.innerHTML = "<p><img src='" +  data.image_url + "' ></p>";
-      best_movie_img.setAttribute("data-id", data.id);
-
-      let best_movie_infos = document.getElementById("best_movie_infos");
-      let best_movie_title = document.createElement("h1");
-      let best_movie_description = document.createElement("p");
-
-      best_movie_title.innerText = data.title;
-      best_movie_infos.appendChild(best_movie_title);
-
-      best_movie_description.innerText = data.description;
-      best_movie_infos.appendChild(best_movie_description);
-
-      let btn = document.getElementsByClassName("button_infos_film");
-      btn[0].addEventListener("click", function () {
-      createFilm(best_movie_img.dataset.id);
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
     })
-      })
-      .catch(function(error) {
-        console.error('Error:', error);
+    .then(function(data) {
+    // Afficher l'image du film avec le meilleur score IBMD
+    let best_movie_img = document.getElementById("best_movie_image");
+    best_movie_img.innerHTML = "<p><img src='" +  data.image_url + "' ></p>";
+    best_movie_img.setAttribute("data-id", data.id);
+    best_movie_img.setAttribute("class", "imgPreview");
+    best_movie_img.addEventListener("click", function() {
+      createMovie(best_movie_img.dataset.id);
     })
-  };
-  
+    // Afficher le titre du film avec le meilleur score IBMD
+    let best_movie_title = document.createElement("h1");
+    // Afficher la description du film avec le meilleur score IBMD
+    let best_movie_description = document.createElement("p");
+    best_movie_title.innerText = data.title;
+    best_movie_infos.appendChild(best_movie_title);
+    best_movie_description.innerText = data.description;
+    best_movie_infos.appendChild(best_movie_description);
+    // Afficher le bouton permettant d'afficher les infos du meilleur film
+    let btn = document.getElementsByClassName("button_infos_movie");
+    btn[0].addEventListener("click", function () {
+      createMovie(best_movie_img.dataset.id);
+  })
+    })
+    // Afficher l'erreur
+    .catch(function(error) {
+      console.error('Error:', error);
+  })
+}
 
-// Création d'une image preview pour le caroussel.
-function showPreview(endUrl, indice, containerId) {
-fetch(mainEntryUrl + endUrl)
+
+// Fonction asynchrone qui recherche l'url des images de films en fonction de leur position dans l'API et de leur genre 
+async function getTopGenreFromPositionImgURL(position, genre) {
+  var page;
+  if (position < 5) {
+    page = "";
+  } else {
+    page = "&page=" + Math.ceil(position / 5);
+    position = (position % 5);
+  }
+  if (genre != "top") {
+    genre = "&genre_contains=" + genre;
+  } else{ genre = ""; }
+  let response = await fetch(mainEntryUrl + "?sort_by=-imdb_score" + genre + "" + page);
+  let data = await response.json();
+  var movie_img_url = data.results[position].image_url;
+  return movie_img_url;
+  }
+
+
+// Fonction asynchrone qui recherche l'id des films en fonction de leur position dans l'API et de leur genre 
+async function getTopGenreFromPositionMovieId(position, genre) {
+  var page;
+  if (position < 5) {
+    page = "";
+  } else {
+    page = "&page=" + Math.ceil(position / 5);
+    position = (position % 5);
+  }
+  if (genre != "top") {
+    genre = "&genre_contains=" + genre;
+  } else{ genre = ""; }
+  let response = await fetch(mainEntryUrl + "?sort_by=-imdb_score" + genre + "" + page);
+  let data = await response.json();
+  var movie_id = data.results[position].id;
+  return movie_id;
+  }
+
+
+// Fonction asynchrone qui permet de faire bouger le caroussel
+async function moveCarousel(genre, index) {
+  let carouselParent = document.getElementsByClassName("parent_" + genre)[0];
+  for (var i = 1; i < 5; i++){
+    var card = carouselParent.children[0].children[parseInt(i)];
+    var new_id = parseInt(card.id) + parseInt(index);
+    if(new_id == -1){
+      new_id = 10;
+    } else if(new_id == 11){
+      new_id = 0;
+    }
+    card.id = new_id;
+    card.src = await getTopGenreFromPositionImgURL(parseInt(new_id), genre);
+  }
+}
+
+
+// Fonction asynchrone qui permet de créer un caroussel
+async function createCarousel(genre) {
+  let carouselParent = document.getElementsByClassName("parent_" + genre)[0];
+  let carouselDiv = document.createElement("div");
+  carouselDiv.setAttribute("class", "carousel");
+  carouselDiv.setAttribute("name", "carousel_" + genre);
+
+  // Création du bouton de gauche
+  let btn_left = document.createElement("button");
+  btn_left.setAttribute("class","nav_button");
+  btn_left.setAttribute("name","btn_left");
+  btn_left.addEventListener('click', function() {
+    moveCarousel(genre, 1);
+  })
+  let btn_img = document.createElement("img");
+  btn_img.setAttribute("class","nav_button_img");
+  btn_img.setAttribute("src", "img/left_btn.png");
+  btn_left.appendChild(btn_img);
+  carouselDiv.appendChild(btn_left);
+
+  // Création d'une carte contenant chaque image pour en afficher 4
+  for (var i = 0; i < 4; i++){
+    let card = document.createElement("img");
+    card.setAttribute("class","card");
+    card.setAttribute("id",i);
+    card.setAttribute("class", "imgPreview");
+    card.setAttribute("src", await getTopGenreFromPositionImgURL(i, genre));
+    card.setAttribute("data-id", await getTopGenreFromPositionMovieId(i, genre));
+    carouselDiv.appendChild(card);
+    card.addEventListener('click', function() {
+      createMovie(card.dataset.id);
+    })
+  }
+
+  // Création du bouton de droite
+  let btn_right = document.createElement("button");
+  btn_right.setAttribute("class","nav_button");
+  btn_right.setAttribute("name","btn_right");
+  btn_right.addEventListener('click', function() {
+    moveCarousel(genre, -1);
+  })
+  let btn_img2 = document.createElement("img");
+  btn_img2.setAttribute("class","nav_button_img");
+  btn_img2.setAttribute("src", "img/right_btn.png");
+  btn_right.appendChild(btn_img2);
+  carouselDiv.appendChild(btn_right);
+  carouselParent.appendChild(carouselDiv);
+}
+
+
+////////////
+/// MAIN ///
+////////////
+
+function main() {
+  // Bloc meilleur film
+  fetch (mainEntryUrl + "?sort_by=-imdb_score")
   .then(function(res) {
     if (res.ok) {
         return res.json();
     }
   })
-  .then(function(data) {   
-    let caroussel_content = document.getElementsByClassName(containerId)[0];
-    let film_img = document.createElement("li");
-    
-    film_img.innerHTML = "<p><img src='" +  data.results[indice].image_url + "'></p>";
-    film_img.setAttribute("data-id", data.results[indice].id);
-    film_img.setAttribute("class", "imgPreview");
-    caroussel_content.appendChild(film_img);
-    
-    film_img.addEventListener("click", function() {
-      createFilm(film_img.dataset.id)
-    })
+  .then(function(data) {
+    let bestMovieUrl = data.results[0].url;
+    showPreviewBestMovie(bestMovieUrl);
   })
-  .then(() => {
-      let carrousel = document.getElementsByClassName(containerId)[0].childNodes;
-        carrousel.forEach(function (currentValue, currentIndex) {
-      currentValue.setAttribute("data-carousselPlace", currentIndex)
-
-      if (currentIndex < 4) {
-        currentValue.style.display = "block"
-      } else {
-        currentValue.style.display = "none"
-      }
-    })
-    
-  })
-  .catch(function(error) {
-      console.log(error);
-    }); 
+  // Création du 1er caroussel (meilleurs films toutes catégories confondues)
+  createCarousel("top");
+  // Création du 2ème caroussel (meilleurs films d'action)
+  createCarousel("action");
+  // Création du 3ème caroussel (meilleurs films comique)
+  createCarousel("comedy");
+  // Création du 4ème caroussel (meilleurs films pour enfant)
+  createCarousel("family");
+  // Création du 5ème caroussel (meilleurs films d'horreur)
+  createCarousel("horror");
 }
-
-
-function createCarousselSection(endUrl1, endUrl2, containerId) {
-
-    // Création des sections d'images
-    for (let movie = 0; movie < 5; movie++) {
-      showPreview(endUrl1, movie, containerId)
-    }
-  
-    for (let movie = 0; movie < 2; movie++) { 
-      showPreview(endUrl2, movie, containerId)
-    }
-}
-
-// Gérer les flèche droite /gauche
-var arrow_right = document.getElementsByClassName('right')
-var arrow_left = document.getElementsByClassName('left')
-
-for(var i=0; i < arrow_right.length; i++){
-  arrow_right[i].addEventListener('click', function() {
-    turn_right(this)
-  })
-  arrow_left[i].addEventListener('click', function() {
-    turn_left(this)
-  })
-}
-
-
-function turn_right(arrow_right){
-    let divParent = arrow_right.parentElement
-  let divCarrousel = divParent.getElementsByTagName('div')[0]
-  let figures = divParent.getElementsByClassName('imgPreview')
-  for(var i = 0; i < figures.length; i++){
-    figures[i].dataset['carousselplace'] = (figures[i].dataset['carousselplace'] + 6 ) % 7
-  }
-  refreshCarrousel(divCarrousel)
-}
-
-
-function turn_left(arrow_left){
-    let divParent = arrow_left.parentElement
-  let divCarrousel = divParent.getElementsByTagName('div')[0]
-  let figures = divParent.getElementsByClassName('imgPreview')
-  for(var i = 0; i < figures.length; i++){
-    figures[i].dataset['carousselplace'] = (figures[i].dataset['carousselplace'] + 1) % 7
-  }
-  refreshCarrousel(divCarrousel)
-}
-
-
-function refreshCarrousel(carrousel){
-  figures = carrousel.getElementsByClassName('imgPreview')
-  for(var i = 0; i < figures.length; i++){
-    if(figures[i].dataset['carousselplace'] < 4){
-      figures[i].style.display = "block"
-    } else {
-      figures[i].style.display = "none"
-    }
-  }
-}
-
-function main() {
-
-    // Bloc meilleur film.
-    fetch (mainEntryUrl + "?sort_by=-imdb_score")
-    .then(function(res) {
-      if (res.ok) {
-          return res.json();
-      }
-    })
-    .then(function(data) {
-      let bestMovieUrl = data.results[0].url
-      showPreviewBestMovie(bestMovieUrl);
-    });
-
-  // 1er caroussel (meilleurs films)
-  createCarousselSection("?sort_by=-imdb_score&page_size=7 ", "?sort_by=-imdb_score&page=2", "images_BM")
-
-  // 2d caroussel (meilleurs films d'action)
-  createCarousselSection("?genre_contains=action&sort_by=-imdb_score", "?genre_contains=action&sort_by=-imdb_score&page=2", "images_A")
-  
-  // 3eme caroussel (meilleurs films thriller)
-  createCarousselSection("?genre_contains=thriller&sort_by=-imdb_score", "?genre_contains=comedy&sort_by=-imdb_score&page=2", "images_C")
-
-  // 4eme caroussel (meilleurs films pour enfants)
-  createCarousselSection("?genre_contains=horror&sort_by=-imdb_score", "?genre_contains=animation&sort_by=-imdb_score&page=2", "images_BB")
-
-  // 5eme caroussel (meilleurs films d'horreur)
-  createCarousselSection("?genre_contains=horror&sort_by=-imdb_score", "?genre_contains=horro&sort_by=-imdb_score&page=2", "images_H")
-}
-
-
 main()
